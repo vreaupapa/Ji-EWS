4f4479677443ca6f4491f575a67c38477762ed89
vreaupapa <birsanionutfv@gmail.com>
2026-05-26 22:22:33 +0300

    Senzori

---
+++ ews-common.h
@@ -0,0 +1,30 @@
+#ifndef EWS_COMMON_H
+#define EWS_COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <signal.h>
+
+#define MAX_SENSORS 5
+#define THRESHOLD_DANGER 7.0 //magnitudinea peste care e nasol
+#define PROJECT_ID 'J'
+
+struct sensor_msg{
+    long mtype;     //tipul mesajului
+    //Orice structura pe care vreau sa o bag intr o coada de mesaje trebuie sa aiba
+    //pe prima pozitie o variabila de tip long numita mtype 
+    //E un sistem de filtrare automat si rapid integrat direct in kernel
+    //De exemplu, daca avem mtype = 1 pemntru rapoarte de cutremur si
+    //mtype = 2 pentru baterie descarcata, thread-urile din Daemon pot zice:
+    //Vreau doar cele care imi raporteaza cutremurul adica mtype = 1
+    int sensor_id; //care senzor trimite
+    double magnitude;
+    char location[32]; //unde e cutremurul
+};
+
+#endif
---
+++ ews-sensor.c
@@ -0,0 +1,76 @@
+#include "ews-common.h"
+#include <time.h>
+
+/*
+    -verificam daca avem numarul de argumente necesar
+    -initializam generator de nuemre aleatoare
+    -generam cheia pe care o vom folosi pentru a comunica cu message queue(ca sa nu scrie toate deodata)
+    key_t key = ftok(cale, argument) - face un ID unic folosind argumentul si hashing
+    -ne conectam la message queue
+    msgget(key, permisiune | IPC_CREAT) (in caz ca nu a fost creat, folosim IPC_CREAT pentru a il crea, altfel, te conectezi direct la ea)
+    -bucla infinita
+        -timp random de dormit deoarece cutremurele nu sunt constante(cred ca o sa pun intre 2 si 7 secunde)
+        -generam cutremure random (80% sa fie mic/20% sa fie periculos)
+        -bagam datele intr un sensor
+        -trimitem datele de la sensor la message queue(neaparat fara marimea lui mtype, acela nu ne trebuie)
+        msgsnd(Message_Queue, &sensor_data, size_data, 0)//0 in caz de succes, -1 in caz de eroare
+*/
+
+int main(int argc, char* argv[]){
+    //-verificam daca avem numarul de argumente necesar
+    if(argc != 2){
+        printf("Eroare!\n Format Utilizare: %s <ID_Senzor(int)>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int id_senzor = atoi(argv[1]);
+    //-initializam generator de nuemre aleatoare
+    srand(time(NULL) ^ (getpid() << 16));
+
+
+    // -generam cheia pe care o vom folosi pentru a comunica cu message queue(ca sa nu scrie toate deodata)
+    // key_t key = ftok(cale, argument) - face un ID unic folosind argumentul si hashing
+    key_t key = ftok(".", PROJECT_ID);
+    if(key == -1){
+        perror("Eroare la generarea cheii cu ftok");
+        exit(EXIT_FAILURE);
+    }
+    //-ne conectam la message queue
+    // msgget(key, permisiune | IPC_CREAT) (in caz ca nu a fost creat, folosim IPC_CREAT pentru a il crea, altfel, te conectezi direct la ea)
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+    if(msgid == -1){
+        perror("Eroare la crearea/conectarea la Message Queue");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("[Senzor %d] Activat si conectat la sistemul EWS.\n", id_senzor);
+    //-bucla infinita
+    while(1){
+        //-timp random de dormit deoarece cutremurele nu sunt constante(cred ca o sa pun intre 2 si 7 secunde)
+        int sleep_timer = (rand() % 6) + 2;
+        sleep(sleep_timer);
+        //-generam cutremure random (80% sa fie mic/20% sa fie periculos)
+        double magn = 0.0;
+        if(rand() % 100 < 80){
+            magn = 2.0 + ((double)rand() / RAND_MAX) * 3.0;
+        } else {
+            magn = 5.0 + ((double)rand() / RAND_MAX) * 3.5;
+        }
+        //-bagam datele intr un sensor
+        struct sensor_msg msg;
+        msg.mtype = 1;
+        msg.sensor_id = id_senzor;
+        msg.magnitude = magn;
+        strcpy(msg.location, "Coasta de Est, JP");
+        //scapam de size ul mtype ului, el nu face parte din datele brute ale mesajului seismului
+        size_t msg_size = sizeof(struct sensor_msg) - sizeof(long);
+        //-trimitem datele de la sensor la message queue(neaparat fara marimea lui mtype, acela nu ne trebuie)
+        // msgsnd(Message_Queue, &sensor_data, size_data, 0)//0 in caz de succes, -1 in caz de eroare
+        if(msgsnd(msgid, &msg, msg_size, 0) == -1){
+            perror("Senzor: Eroare la msgsnd (nu am putut trimite mesajul)");
+        } else {
+            printf("[Senzor %d] A detectat activitate seismica: %.1f grade. Raport trimis! \n", id_senzor, magn);
+        }
+    }
+    return 0;
+}
